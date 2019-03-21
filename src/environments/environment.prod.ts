d97a6bab0101f948fe4d8d26d9d8ed93e583d787
Deepak Kumar Dewani <kumard8308887772@gmail.com>
2019-03-21 15:27:49 +0530

    ADD: env var

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  serverUrl: 'https://www.anandvrindavan.com/api',
+  youtubeUrl: 'https://www.googleapis.com/youtube/v3/',
+  YOUTUBE_KEY: 'AIzaSyCkEjR4v9GJ-l-MNWEToExy56TMzsUKsUM',
+  PLAYLIST_ID: 'UUus5fDaVo14jrBEZQ-98WvA'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  serverUrl: 'http://localhost:3050',
+  youtubeUrl: 'https://www.googleapis.com/youtube/v3/',
+  YOUTUBE_KEY: 'AIzaSyCkEjR4v9GJ-l-MNWEToExy56TMzsUKsUM',
+  PLAYLIST_ID: 'UUus5fDaVo14jrBEZQ-98WvA'
 };
 
 /*
