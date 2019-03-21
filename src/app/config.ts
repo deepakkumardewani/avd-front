dcd4c730257a57e549174c995d7562bb2071ed01
Deepak Kumar Dewani <kumard8308887772@gmail.com>
2019-03-21 15:29:27 +0530

    ADD: swiper config

---
+++ src/app/config.ts
@@ -0,0 +1,71 @@
+export const config = {
+  swiper: {
+    banner: {
+      slidesPerView: 1,
+      autoplay: {
+        disableOnInteraction: false,
+        delay: 4000
+      },
+      // effect: 'fade',
+      spaceBetween: 1,
+      speed: 2000,
+      initialSlide: 0,
+      centeredSlides: true,
+      // width: 270,
+      // slidesOffsetBefore: 50
+    }
+  },
+  galleryOptions: {
+
+  },
+  events: [
+    {
+      name: 'Gaur Purnima',
+      desc: 'Gaur Purnima is a Gaudiya Math Festival, which is celebrated on the Divine appearance of Shri Chaitanya Mahaprabhu...',
+      image: 'SMB.jpg',
+      date: 'Friday, March 1, 2019 / 8:00 AM : 1:00 PM'
+    },
+    {
+      name: 'Nirjila Ekadashi',
+      desc: '30th October 1962… the appearance day of a Great Personality of today’s time, who appeared for uplifting the fallen souls from this kaliyug to Golok Dham...',
+      image: 'SMB.jpg',
+      date: 'Friday, March 1, 2019 / 8:00 AM : 1:00 PM'
+    },
+    {
+      name: 'Guru Purnima',
+      desc: 'The Spiritual Master is equated with GOD and always regarded as the holy link between the living entities and GOD...',
+      image: 'SMB.jpg',
+      date: 'Friday, March 1, 2019 / 8:00 AM : 1:00 PM'
+    },
+    {
+      name: 'Shri Krishna Janamashtami',
+      desc: 'The Auspicious day of the appearance of Lord Krishna is celebrated as Krishna Janmashtami in the whole world...',
+      image: 'SMB.jpg',
+      date: 'Friday, March 1, 2019 / 8:00 AM : 1:00 PM'
+    },
+    {
+      name: 'Radha Ashtami',
+      desc: 'Radha Ashtami is the divine appearance of Shrimati Radha Rani, the eternal consort of the Supreme personality of Godhead...',
+      image: 'SMB.jpg',
+      date: 'Friday, March 1, 2019 / 8:00 AM : 1:00 PM'
+    },
+    {
+      name: 'Srila Bhaktipada Vyas Puja',
+      desc: '30th October 1962… the appearance day of a Great Personality of today’s time, who appeared for uplifting the fallen souls from this kaliyug to Golok Dham...',
+      image: 'SMB.jpg',
+      date: 'Friday, March 1, 2019 / 8:00 AM : 1:00 PM'
+    },
+    {
+      name: 'Madhusudan Bapuji Vyas Puja',
+      desc: '30th October 1962… the appearance day of a Great Personality of today’s time, who appeared for uplifting the fallen souls from this kaliyug to Golok Dham...',
+      image: 'SMB.jpg',
+      date: 'Friday, March 1, 2019 / 8:00 AM : 1:00 PM'
+    },
+    {
+      name: 'Rath Yatra',
+      desc: 'Jagannath Ratha Yatra is a major Hindu festival associated with Lord Jagannath (avatar of Lord Vishnu) held at Puri in India during the months of June or July...',
+      image: 'SMB.jpg',
+      date: 'Friday, March 1, 2019 / 8:00 AM : 1:00 PM'
+    }
+  ]
+};
