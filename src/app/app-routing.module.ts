8ab5c57a31f83ea38a6aa072bec2e74aec294042
Deepak Kumar Dewani <kumard8308887772@gmail.com>
2019-03-21 15:28:58 +0530

    ADD: initialize app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,30 @@
+import { QuotesComponent } from './quotes/quotes.component';
+import { AboutComponent } from './about/about.component';
+import { ApplicationComponent } from './application/application.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EventsComponent } from './events/events.component';
+import { EventDetailComponent } from './event-detail/event-detail.component';
+import { ContactComponent } from './contact/contact.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { LecturesComponent } from './lectures/lectures.component';
+import { BooksComponent } from './books/books.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent, data: {title: 'Home'} },
+  { path: 'about', component: AboutComponent, data: {title: 'About'} },
+  { path: 'quotes', component: QuotesComponent, data: {title: 'Quotes'} },
+  { path: 'events', component: EventsComponent, data: {title: 'Events'} },
+  { path: 'events/:name', component: EventDetailComponent, data: {title: 'EventDetail'} },
+  { path: 'gallery', component: GalleryComponent, data: {title: 'Gallery'} },
+  { path: 'application', component: ApplicationComponent, data: {title: 'Application'} },
+  { path: 'contact', component: ContactComponent, data: {title: 'Contact'} },
+  { path: 'lectures', component: LecturesComponent, data: {title: 'Lectures'} },
+  { path: 'books', component: BooksComponent, data: {title: 'Books'} },
+  { path: '**', redirectTo: 'home' }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,81 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
 import { AppRoutingModule } from './app-routing.module';
+import { NgxGalleryModule } from 'ngx-gallery';
+
 import { AppComponent } from './app.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { EventsComponent } from './events/events.component';
+import { EventDetailComponent } from './event-detail/event-detail.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { ApplicationComponent } from './application/application.component';
+import { AboutComponent } from './about/about.component';
+
+import { ShortenPipe } from './shorten.pipe';
+
+import {
+  LecturesComponent,
+  AudioDialogComponent
+} from './lectures/lectures.component';
+import { BooksComponent } from './books/books.component';
+
+
+import { HelperService } from './helper.service';
+
+import { SwiperModule } from 'ngx-swiper-wrapper';
+import { SWIPER_CONFIG } from 'ngx-swiper-wrapper';
+import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
+import { HttpClientModule } from '@angular/common/http';
+import {NgxPaginationModule} from 'ngx-pagination';
+
+const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
+  direction: 'horizontal',
+  slidesPerView: 'auto'
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    GalleryComponent,
+    EventsComponent,
+    EventDetailComponent,
+    ContactComponent,
+    HomeComponent,
+    ApplicationComponent,
+    AboutComponent,
+    LecturesComponent,
+    AudioDialogComponent,
+    BooksComponent,
+    QuotesComponent,
+    ShortenPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    SwiperModule,
+    HttpClientModule,
+    NgxGalleryModule,
+    NgxPaginationModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: SWIPER_CONFIG,
+      useValue: DEFAULT_SWIPER_CONFIG
+    },
+    HelperService
+  ],
+  entryComponents: [AudioDialogComponent],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
