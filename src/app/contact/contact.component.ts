4e8acedd78ce72d0596fc1525eb9203a16c9d480
Deepak Kumar Dewani <kumard8308887772@gmail.com>
2020-04-09 18:28:33 +0530

    ADD: contact comp

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HelperService } from './../helper.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +9,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  constructor(private helper: HelperService) { }
+
+  isSubmitDisabled = false;
+
+  showAlert = false;
+
+  name = '';
+  email = '';
+  message = '';
+
+  contactForm: FormGroup;
+
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+
+
 
   ngOnInit() {
+    this.contactForm = new FormGroup({
+      name: new FormControl('', [
+        Validators.required
+      ]),
+      message: new FormControl('', [
+        Validators.required
+      ])
+    });
+  }
+
+  submit() {
+    this.isSubmitDisabled = true;
+    const { name, message } = this.contactForm.value;
+    const email = this.emailFormControl.value;
+
+    const data = { name, email, message };
+
+    this.helper.submitContactUs(data).subscribe(_ => {
+      this.showAlert = true;
+      setTimeout(() => {
+        this.showAlert = false;
+        this.isSubmitDisabled = false;
+        this.contactForm.reset();
+        this.emailFormControl.reset();
+      }, 2000);
+
+    });
+  }
+
+  isValid() {
+    return this.contactForm.status !== 'INVALID' && this.emailFormControl.status !== 'INVALID';
   }
 
 }
