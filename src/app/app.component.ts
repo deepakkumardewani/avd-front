7ad49ac9c8fc81232cebddf3341b1ec4937452aa
Deepak Kumar Dewani <kumard8308887772@gmail.com>
2020-04-13 15:00:36 +0530

    ADD: check for navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, Event, NavigationEnd } from '@angular/router';
+
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private router: Router) {}
   title = 'avd-front';
+
+  ngOnInit() {
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+          // Hide loading indicator
+          $('.navbar-collapse').collapse('hide');
+      }
+  });
+    // $('.navbar-nav>li>a').on('click', function() {
+    //   $('.navbar-collapse').collapse('hide');
+    // });
+  }
+
 }
